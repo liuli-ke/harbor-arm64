73c77fe79d7ca7febd0c61faa3e6f9c92c533473
流漓·客 <2743266947@qq.com>
2026-08-13 15:31:31 +0800

    ci: 增加镜像tag带v的action

---
+++ build-harbor.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# 配置在build.yml下面进行修改
+if [[ -z $version ]]
+then
+    version=v2.15.1
+fi
+
+# 强制 Docker 使用 linux/arm64 平台构建
+export DOCKER_DEFAULT_PLATFORM=linux/arm64
+
+echo version: $version
+
+git clone --branch ${version} https://github.com/goharbor/harbor.git
+
+cd harbor
+
+# 指定版本需要替换photon基础镜像引用
+# 如果有多个版本，可以用 | 分隔，例如 "2.15.1|v2.15.1"
+specified_versions="v2.15.1|v2.15.1-rc1|v2.15.1-rc2"
+case ${version} in
+    $specified_versions)
+        find . -name Dockerfile.base | xargs sed -i 's#goharbor/photon:5.0#photon:5.0#g'
+        ;;
+esac
+
+sed -i 's#Linux-64bit.tar.gz#Linux-ARM64.tar.gz#g' ./Makefile
+
+sed -i "s#VERSIONTAG=dev#VERSIONTAG=${version}#g" ./Makefile
+sed -i "s#BASEIMAGETAG=dev#BASEIMAGETAG=${version}#g" ./Makefile
+sed -i "s#PULL_BASE_FROM_DOCKERHUB=true#PULL_BASE_FROM_DOCKERHUB=false#g" ./Makefile
+sed -i "s#BUILDBIN=false#BUILDBIN=true#g" ./Makefile
+sed -i 's#--no-cache##g' make/photon/Makefile
+sed -i 's#GOARCH=amd64#GOARCH=arm64#g' make/photon/exporter/Dockerfile
+
+sed -i 's#swagger_linux_amd64#swagger_linux_arm64#g' tools/swagger/Dockerfile
+# 调整版本信息
+ABOUT_DIALOG_PATH='src/portal/src/app/shared/components/about-dialog/about-dialog.component.html'
+CUSTOM_VERSION_SUFFIX='Liulike'
+sed -i "s/{{ 'ABOUT.VERSION' | translate }} {{ version }}/{{ 'ABOUT.VERSION' | translate }} {{ version }} - $CUSTOM_VERSION_SUFFIX/g" "$ABOUT_DIALOG_PATH"
+
+compare_versions() {
+    IFS='.' read -r -a version1 <<< "$1"
+    IFS='.' read -r -a version2 <<< "$2"
+
+    for i in "${!version1[@]}"; do
+        if (( ${version1[i]} < ${version2[i]} )); then
+            echo "1" # 表示version1早于version2
+            return 1
+        elif (( ${version1[i]} > ${version2[i]} )); then
+            echo "0" # 表示version1晚于version2
+            return 0
+        fi
+    done
+
+    # 如果循环结束还没有返回，说明两个版本号相等
+    echo "0"
+}
+
+
+if compare_versions 2.12.0 ${version#v}
+then
+    # 2.12.2版本之前需要替换
+    echo 替换
+    sed -i 's#SPECTRAL_VERSION=v6.1.0#SPECTRAL_VERSION=v6.11.0#g' ./Makefile
+    sed -i 's#SPECTRAL_VERSION/spectral-linux #SPECTRAL_VERSION/spectral-linux-arm64 #g' ./tools/spectral/Dockerfile
+fi
+
+echo "ignore-warnings ARM64-COW-BUG" >> ./make/photon/redis/redis.conf
+
+cat > make/photon/redis/Dockerfile << EOF
+FROM redis
+VOLUME /var/lib/redis
+WORKDIR /var/lib/redis
+COPY ./make/photon/redis/docker-healthcheck /usr/bin/
+COPY ./make/photon/redis/redis.conf /etc/redis.conf
+RUN chmod +x /usr/bin/docker-healthcheck \\
+    && chown redis:redis /usr/bin/docker-healthcheck \\
+    && chown redis:redis /etc/redis.conf
+
+HEALTHCHECK CMD ["docker-healthcheck"]
+USER redis
+CMD ["redis-server", "/etc/redis.conf"]
+EOF
+
+# 构建镜像 (包括 trivy-adapter、harbor-exporter)
+make package_offline TRIVYFLAG=true EXPORTERFLAG=true
+
+ls
